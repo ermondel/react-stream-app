7fba1f675e2111ce3bb7bda7f555f084bf24fd99
ermondel <ermondel@gmail.com>
2020-12-02 12:11:04 +0200

    feat: checking media server availability

--- src/App.js
+++ src/App.js
@@ -1,5 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const App = () => <div className='app'>App</div>;
+class App extends Component {
+  state = {
+    mediaServerIsAvailable: false,
+    progressBarMessage: '',
+  };
+
+  pingMediaServer() {
+    axios.get('http://localhost:8000').catch((error) => {
+      if (!error.message.includes('Network Error')) {
+        this.setState({ mediaServerIsAvailable: true });
+      }
+    });
+  }
+
+  progressBarActive() {
+    if (!this.state.mediaServerIsAvailable) {
+      this.setState({ progressBarMessage: 'animate' });
+
+      this.pingMediaServer();
+
+      setTimeout(() => this.progressBarStatic(), 8000);
+    }
+  }
+
+  progressBarStatic() {
+    if (!this.state.mediaServerIsAvailable) {
+      this.setState({ progressBarMessage: 'static' });
+
+      setTimeout(() => this.progressBarActive(), 8000);
+    }
+  }
+
+  componentDidMount() {
+    if (!this.state.mediaServerIsAvailable) {
+      this.progressBarActive();
+    }
+  }
+
+  render() {
+    if (this.state.mediaServerIsAvailable) {
+      return <div>Video Player</div>;
+    } else {
+      return <div>{this.state.progressBarMessage}</div>;
+    }
+  }
+}
 
 export default App;
